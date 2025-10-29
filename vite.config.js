20075c22ed6c88e96a595559028fba266c5ef1de
stijnvandam <Stijn_vandam@hotmail.com>
2025-10-29 21:05:09 +0100

    Fix Vite routing for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // allows external access
-    port: 5173,
-    strictPort: true,
-    allowedHosts: ['cristi-unexhumed-britni.ngrok-free.dev'], // 👈 replace with your current ngrok host
-    cors: true,
-  },
-})
+  base: "/", // ensures routing works on Vercel
+  build: {
+    outDir: "dist"
+  }
+});
